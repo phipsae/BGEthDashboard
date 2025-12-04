d1ad483396dbe11cb537a5a543569aee6a5c3b7e
phipsae <philip.krause88@yahoo.de>
2025-12-04 15:38:28 -0300

    Initial Commit

---
+++ BGEthGasWidget/BGEthGasWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGEthGasWidgetApp.swift
+//  BGEthGasWidget
+//
+//  Created by Philip on 04.12.25.
+//
+
+import SwiftUI
+
+@main
+struct BGEthGasWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
